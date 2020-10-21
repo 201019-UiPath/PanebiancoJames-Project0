4e5e3180293b29bf079078b5cdfbca0dcacb5833
James Panebianco <daxterscool41@gmail.com>
2020-10-21 14:39:49 -0400

    Setup design for Project0

---
+++ GameKingdom/GameKingdomDB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GameKingdomDB
+{
+    public class Class1
+    {
+    }
+}
---
+++ GameKingdom/GameKingdomLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GameKingdomLib
+{
+    public class Class1
+    {
+    }
+}
